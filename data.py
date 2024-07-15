ea3ee15af9115618c1184de1269b7d01701df221
“mariaagromero” <mariaromeroecom@gmail.com>
2024-07-15 17:41:11 +1000

    Revisiones proyecto finished

--- data.py
+++ data.py
@@ -1,7 +1,6 @@
-urban_routes_url = 'https://cnt-331ca299-35e1-4de3-bf9f-96222f5511b8.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-386fac5b-810b-4524-ad49-3c42f71c136c.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
 card_number, card_code = '1234 5678 9100', '111'
 message_for_driver = 'Muéstrame el camino al museo'
-
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import time
-
-
 import data
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 # no modificar
@@ -14,7 +13,6 @@ def retrieve_phone_code(driver) -> str:
     Utilízalo cuando la aplicación espere el código de confirmación para pasarlo a tus pruebas.
     El código de confirmación del teléfono solo se puede obtener después de haberlo solicitado en la aplicación."""
     import json
-    import time
     from selenium.common import WebDriverException
     code = None
     for i in range(10):
@@ -39,7 +37,8 @@ class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
     button_round = (By.CLASS_NAME, "button.round")
-    comfort_tariff = (By.XPATH, '//div[contains(@class, "tcard") and .//div[contains(@class, "tcard-title") and text()="Comfort"]]')
+    #Selector CSS Agregado Aqui:
+    comfort_tariff = (By.CSS_SELECTOR, 'div.tcard div.tcard-title')
     phone_button = (By.XPATH, "//div[@class='np-button']//div[@class='np-text' and text()='Número de teléfono']")
     phone_field = (By.ID, 'phone')
     next_button = (By.XPATH, "//button[@type='submit' and @class='button full' and text()='Siguiente']")
@@ -56,7 +55,6 @@ class UrbanRoutesPage:
     mantas_panuelos_button = (By.CLASS_NAME, 'slider')
     plus_button = (By.CLASS_NAME, 'counter-plus')
     pedir_taxi_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[4]/button')
-
 
     def __init__(self, driver):
         self.driver = driver
@@ -76,8 +74,14 @@ class UrbanRoutesPage:
         button_round_element.click()
 
     def select_comfort_tariff(self):
-        comfort_tariff_element = self.driver.find_element(*self.comfort_tariff)
-        comfort_tariff_element.click()
+        # Encuentra todos los elementos que coinciden con el selector CSS
+        elements = self.driver.find_elements(*self.comfort_tariff)
+        for element in elements:
+            # Verifica si el texto del elemento es "Comfort"
+            if element.text == "Comfort":
+                # Hace clic en el elemento padre
+                element.find_element(By.XPATH, "./..").click()
+                break
 
     def select_phone_button(self):
         phone_button_element = self.driver.find_element(*self.phone_button)
@@ -155,6 +159,14 @@ class UrbanRoutesPage:
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
+    def get_phone(self):
+        return self.driver.find_element(*self.phone_field).get_property('value')
+
+    def get_card_number(self):
+        return self.driver.find_element(*self.add_card_number).get_property('value')
+
+    def get_CVV_number(self):
+        return self.driver.find_element(*self.add_card_cvv).get_property('value')
 
 class TestUrbanRoutes:
     driver = None
@@ -176,48 +188,134 @@ class TestUrbanRoutes:
         self.driver.get(data.urban_routes_url)
         self.routes_page = UrbanRoutesPage(self.driver)
 
-    def test_urban_route_page(self):
+    def test_urban_route_page(self, comfort_tariff_element=None):
         time.sleep(2)
+        #Ingresa direcciones en el campo FROM y TO
         self.routes_page.set_from(data.address_from)
         assert self.routes_page.get_from() == data.address_from, f"Expected 'from' field to be '{data.address_from}', but got '{self.routes_page.get_from()}'"
         time.sleep(1)
         self.routes_page.set_to(data.address_to)
         assert self.routes_page.get_to() == data.address_to, f"Expected 'to' field to be '{data.address_to}', but got '{self.routes_page.get_to()}'"
         time.sleep(1)
+
+        # Hace clic en el boton Pedir taxi
         self.routes_page.call_taxi()
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.routes_page.button_round)
+        )
+        button_round_element = self.driver.find_element(*self.routes_page.button_round)
+        assert button_round_element.is_enabled() and button_round_element.is_displayed(), "The button is not clickable"
         time.sleep(1)
+
+        #Selecciona el boton Comfort Tariff
         self.routes_page.select_comfort_tariff()
+        WebDriverWait(self.driver, timeout=10).until(
+            EC.element_to_be_clickable(self.routes_page.comfort_tariff)
+        )
+        comfort_tariff_element = self.driver.find_element(*self.routes_page.comfort_tariff)
+        assert comfort_tariff_element.is_enabled() and comfort_tariff_element.is_displayed(), "The button is not clickable"
         time.sleep(1)
+
+        #selecciona el boton de agregar numero de telefono
         self.routes_page.select_phone_button()
+        WebDriverWait(self.driver, timeout= 10).until(
+            EC.element_to_be_clickable(self.routes_page.phone_button)
+        )
+        phone_button_element = self.driver.find_element(*self.routes_page.phone_button)
+        assert phone_button_element.is_enabled() and phone_button_element.is_displayed(), "The button is not clickable"
+        time.sleep(2)
+
+        #Agregar numero de telefono
         self.routes_page.set_phone(data.phone_number)
         time.sleep(1)
-        self.routes_page.select_next_button()
+        phone_field_value = self.driver.find_element(*self.routes_page.phone_field).get_property('value')
+        assert phone_field_value == data.phone_number, f"Expected phone number to be '{data.phone_number}', but got '{phone_field_value}'"
         time.sleep(2)
+
+        # Hacer clic en el boton siguiente y retrieve codigo de SMS
+        self.routes_page.select_next_button()
         sms_code = retrieve_phone_code(self.driver)
         self.routes_page.set_code(sms_code)
         time.sleep(2)
+        sms_code_field_value = self.driver.find_element(*self.routes_page.sms_code).get_property('value')
+        assert sms_code_field_value == sms_code, f"Expected SMS code to be '{sms_code}', but got '{sms_code_field_value}'"
+        time.sleep(2)
+
+        # Hacer clic en el boton confirmar
         self.routes_page.select_confirm_button()
-        time.sleep(3)
+        time.sleep(2)
+
+        # Hacer clic en el botón "Método de pago"
         self.routes_page.select_payment_method_button()
         time.sleep(2)
+        payment_method_button_element = self.driver.find_element(*self.routes_page.payment_method_button)
+        assert payment_method_button_element.is_enabled() and payment_method_button_element.is_displayed(), "Payment method button was not clicked successfully."
+        time.sleep(2)
+
+        # Hacer clic en el botón "Agregar tarjeta"
         self.routes_page.select_add_card_button()
         time.sleep(2)
+
+        # Ingresar el número de tarjeta
         self.routes_page.select_add_card_number(data.card_number)
         time.sleep(3)
+        add_card_number_element = self.driver.find_element(*self.routes_page.add_card_number)
+        card_number_value = add_card_number_element.get_attribute('value')
+        assert card_number_value == data.card_number, f"Expected card number to be '{data.card_number}', but got '{card_number_value}'"
+        time.sleep(2)
+
+        # Ingresar el CVV de la tarjeta
         self.routes_page.select_add_card_cvv(data.card_code)
         time.sleep(2)
-        self.routes_page.select_agregar_button()
+        add_card_cvv_element = self.driver.find_element(*self.routes_page.add_card_cvv)
+        assert add_card_cvv_element.get_property(
+            'value') == data.card_code, f"Expected card CVV to be '{data.card_code}', but got '{add_card_cvv_element.get_property('value')}'."
         time.sleep(2)
+
+        # Hacer clic en el botón "Agregar"
+        self.routes_page.select_agregar_button()
+        WebDriverWait(self.driver, timeout=10).until(EC.element_to_be_clickable(self.routes_page.close_popup_button))
+        agregar_button_element = self.driver.find_element(*self.routes_page.agregar_button)
+        time.sleep(2)
+
+        # Cerrar el popup
         self.routes_page.select_close_popup_button()
         time.sleep(2)
+
+        # Seleccionar el campo de mensaje
         self.routes_page.select_click_message_field()
         time.sleep(2)
+        click_message_field_element = self.driver.find_element(*self.routes_page.click_message_field)
+        assert click_message_field_element.is_enabled() and click_message_field_element.is_displayed(), "Message field was not clicked successfully."
+        time.sleep(2)
+
+        # Ingresar el mensaje para el conductor
         self.routes_page.set_message_field(data.message_for_driver)
         time.sleep(2)
+        message_field_element = self.driver.find_element(*self.routes_page.message_field)
+        assert message_field_element.get_property(
+            'value') == data.message_for_driver, f"Expected message for driver to be '{data.message_for_driver}', but got '{message_field_element.get_property('value')}'."
+        time.sleep(2)
+
+        # Seleccionar el botón de mantas y pañuelos
         self.routes_page.select_mantas_panuelos_button()
         time.sleep(2)
+        mantas_panuelos_button_element = self.driver.find_element(*self.routes_page.mantas_panuelos_button)
+        assert mantas_panuelos_button_element.is_enabled() and mantas_panuelos_button_element.is_displayed(), "Mantas y pañuelos button was not clicked successfully."
+        time.sleep(2)
+
+        # Seleccionar el botón de más
         self.routes_page.select_plus_button()
         time.sleep(2)
+        plus_button_element = self.driver.find_element(*self.routes_page.plus_button)
+        assert plus_button_element.is_enabled() and plus_button_element.is_displayed(), "Plus button was not clicked successfully."
+        time.sleep(2)
+
+        # Hacer clic en el botón "Pedir taxi"
         self.routes_page.select_pedir_taxi_button()
-        time.sleep(45)
+        time.sleep(5)
+        pedir_taxi_button_element = self.driver.find_element(*self.routes_page.pedir_taxi_button)
+        assert pedir_taxi_button_element.is_enabled() and pedir_taxi_button_element.is_displayed(), "Pedir taxi button was not clicked successfully."
+        time.sleep(5)
+
 
